4d24e6b612f6982402c5f1f028830703c2082ed3
zhihui.li <zhihui.li@zhihuilis-MacBook-Pro.local>
2019-01-23 11:57:46 +0800

    refactor sample

--- flower.common/src/test/java/com/ly/train/flower/common/sample/one/Sample.java
+++ flower.common/src/test/java/com/ly/train/flower/common/sample/one/Sample.java
@@ -1,14 +1,12 @@
 package com.ly.train.flower.common.sample.one;
 
 import com.ly.train.flower.common.actor.ServiceFacade;
-import com.ly.train.flower.common.service.ServiceFactory;
-import com.ly.train.flower.common.service.ServiceFlow;
-import com.ly.train.flower.common.service.ServiceLoader;
+import com.ly.train.flower.common.service.EnvBuilder;
 
 public class Sample {
 
   public static void main(String[] args) throws Exception {
-    buildServiceEnv();
+    EnvBuilder.buildEnv();
 
     Message2 m2 = new Message2(10, "Zhihui");
     Message1 m1 = new Message1();
@@ -20,24 +18,5 @@ public class Sample {
     System.exit(0);
   }
 
-  public static void buildServiceEnv() {
-    ServiceFactory.registerService("service1",
-        ServiceLoader.getInstance().loadService("com.ly.train.flower.common.sample.one.Service1"));
-    ServiceFactory.registerService("service2",
-        ServiceLoader.getInstance().loadService("com.ly.train.flower.common.sample.one.Service2"));
-    ServiceFactory.registerService("service3",
-        ServiceLoader.getInstance().loadService("com.ly.train.flower.common.sample.one.Service3"));
-    ServiceFactory.registerService("service4",
-        ServiceLoader.getInstance().loadService("com.ly.train.flower.common.sample.one.Service4"));
-    ServiceFactory.registerService("service5",
-        ServiceLoader.getInstance().loadService("com.ly.train.flower.common.service.JointService"));
-
-    ServiceFlow.buildFlow("sample", "service1", "service2");
-    ServiceFlow.buildFlow("sample", "service1", "service3");
-    ServiceFlow.buildFlow("sample", "service2", "service5");
-    ServiceFlow.buildFlow("sample", "service3", "service5");
-    ServiceFlow.buildFlow("sample", "service5", "service4");
-    ServiceFlow.buildFlow("sample", "service4", "service1");
-  }
 
 }
