d9473bd92551df9ed99f4e87a98335c2046d96e6
lingxiezfy <1241948182@qq.com>
2019-03-01 16:19:25 +0800

    增加Web对post提交的json数据提取

--- flower.common/src/main/java/com/ly/train/flower/common/service/web/Web.java
+++ flower.common/src/main/java/com/ly/train/flower/common/service/web/Web.java
@@ -1,10 +1,12 @@
 package com.ly.train.flower.common.service.web;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.AsyncContext;
 import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
 
 public class Web {
 
@@ -37,5 +39,24 @@ public class Web {
 
   public String getParameter(String para) {
     return sr.getParameter(para);
+  }
+
+  /**
+   * Get the JSON data submitted by the post method
+   * @return String / null
+   */
+  public String getJsonString() throws IOException{
+    HttpServletRequest httpSr = (HttpServletRequest)sr;
+    if(!httpSr.getMethod().equalsIgnoreCase("POST") || null == httpSr.getContentType()){
+      return null;
+    }
+    if(!httpSr.getContentType().toLowerCase().contains("application/json")){
+      return null;
+    }
+    InputStream is = httpSr.getInputStream();
+    byte[] b = new byte[httpSr.getContentLength()];
+    int len = is.read(b,0,httpSr.getContentLength());
+    is.close();
+    return len <= 0 ? null:new String(b,httpSr.getCharacterEncoding());
   }
 }
