4a0749a9add13b054fe72565506f6c7ad19b3ece
leeyazhou <lee_yazhou@163.com>
2019-03-22 19:12:17 +0800

    apache license

--- flower.sample/src/main/java/com/ly/train/flower/common/sample/textflow/Service1.java
+++ flower.sample/src/main/java/com/ly/train/flower/common/sample/textflow/Service1.java
@@ -1,15 +1,17 @@
 /**
  * Copyright © 2019 同程艺龙 (zhihui.li@ly.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.ly.train.flower.common.sample.textflow;
 
@@ -20,6 +22,7 @@ import com.ly.train.flower.logging.LoggerFactory;
 
 public class Service1 implements Service<Message1, Message2> {
   static final Logger logger = LoggerFactory.getLogger(Service1.class);
+
   @Override
   public Message2 process(Message1 message, ServiceContext context) {
     logger.info("处理任务 ： {}", context.getId());
