942b371e5929590bf48f093dc100b9c3617b3ab3
lingxiezfy <1241948182@qq.com>
2019-04-12 09:51:08 +0800

    测试AggregateService，发现问题

--- flower.sample/src/main/java/com/ly/train/flower/common/sample/aggregate/controller/AggregateController.java
+++ flower.sample/src/main/java/com/ly/train/flower/common/sample/aggregate/controller/AggregateController.java
@@ -1,20 +1,15 @@
 package com.ly.train.flower.common.sample.aggregate.controller;
 
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
+import com.ly.train.flower.common.annotation.Flower;
+import com.ly.train.flower.common.sample.aggregate.service.*;
+import com.ly.train.flower.web.spring.FlowerController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.ly.train.flower.common.annotation.Flower;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceBegin;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceForkA1;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceForkA2;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceForkA3;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceForkB1;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceForkB2;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceReceiveA;
-import com.ly.train.flower.common.sample.aggregate.service.ServiceReceiveAB;
-import com.ly.train.flower.web.spring.FlowerController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
  * @author: fengyu.zhang
@@ -22,6 +17,10 @@ import com.ly.train.flower.web.spring.FlowerController;
 @RestController
 @Flower(value = "aggregate",serviceName = "ServiceBegin")
 public class AggregateController extends FlowerController {
+
+    @Autowired
+//    FlowerFactory flowerFactory;
+
     @RequestMapping(value = "/test/aggregate/{id}")
     public void process(@PathVariable Integer id, HttpServletRequest req) throws IOException {
         doProcess(id, req);
@@ -32,10 +31,17 @@ public class AggregateController extends FlowerController {
         getServiceFlow().buildFlow(ServiceBegin.class, ServiceForkA1.class);
         getServiceFlow().buildFlow(ServiceBegin.class, ServiceForkA2.class);
         getServiceFlow().buildFlow(ServiceBegin.class, ServiceForkA3.class);
+//        flowerFactory.getServiceFactory().registerService("ag1", AggregateService.class);
+
         getServiceFlow().buildFlow(ServiceForkA1.class, ServiceReceiveA.class);
         getServiceFlow().buildFlow(ServiceForkA2.class, ServiceReceiveA.class);
         getServiceFlow().buildFlow(ServiceForkA3.class, ServiceReceiveA.class);
-        // 第二个分叉
+//        getServiceFlow().buildFlow("ServiceForkA1", "ag1");
+//        getServiceFlow().buildFlow("ServiceForkA1", "ag1");
+//        getServiceFlow().buildFlow("ServiceForkA1", "ag1");
+
+
+//        // 第二个分叉
         getServiceFlow().buildFlow(ServiceReceiveA.class, ServiceForkB1.class);
         getServiceFlow().buildFlow(ServiceReceiveA.class, ServiceForkB2.class);
         getServiceFlow().buildFlow(ServiceReceiveA.class, ServiceReceiveAB.class);
