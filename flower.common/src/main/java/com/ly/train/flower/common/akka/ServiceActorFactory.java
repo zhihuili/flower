541ece8ba5dd69a067f57219dde0dfd32d486396
leeyazhou <lee_yazhou@163.com>
2019-05-09 18:54:46 +0800

    SupervisorActor actor类型改为system

--- flower.common/src/main/java/com/ly/train/flower/common/akka/ServiceActorFactory.java
+++ flower.common/src/main/java/com/ly/train/flower/common/akka/ServiceActorFactory.java
@@ -108,9 +108,8 @@ public class ServiceActorFactory extends AbstractLifecycle {
       actorWrapper = serviceActorCache.get(cacheKey);
       if (actorWrapper == null) {
         if (serviceConfig.isLocal()) {
-          ActorRef actorRef =
-              getActorContext().actorOf(
-                  ServiceActor.props(serviceName, flowerFactory, index).withDispatcher("dispatcher"), cacheKey);
+          ActorRef actorRef = getActorContext()
+              .actorOf(ServiceActor.props(serviceName, flowerFactory, index).withDispatcher("dispatcher"), cacheKey);
           actorWrapper = new ActorRefWrapper(actorRef).setServiceName(serviceName);
         } else {
           // "akka.tcp://flower@127.0.0.1:2551/user/$a"
@@ -127,9 +126,10 @@ public class ServiceActorFactory extends AbstractLifecycle {
         serviceActorCache.put(cacheKey, actorWrapper);
       }
     } catch (Exception e) {
-      throw new FlowerException("fail to create flowerService, flowName : " + serviceConfig.getFlowName()
-          + ", serviceName : " + serviceName + ", serviceClassName : "
-          + serviceConfig.getServiceMeta().getServiceClassName(), e);
+      throw new FlowerException(
+          "fail to create flowerService, flowName : " + serviceConfig.getFlowName() + ", serviceName : " + serviceName
+              + ", serviceClassName : " + serviceConfig.getServiceMeta().getServiceClassName(),
+          e);
     } finally {
       actorLock.unlock();
     }
@@ -271,11 +271,12 @@ public class ServiceActorFactory extends AbstractLifecycle {
     return serviceRouter;
   }
 
-  public ActorRef getSupervierActor() {
+  private ActorRef getSupervierActor() {
     if (supervierActor == null) {
       synchronized (this) {
         if (supervierActor == null) {
-          this.supervierActor = getActorSystem().actorOf(SupervisorActor.props(this), "flower");
+          this.supervierActor = getActorSystem().systemImpl().systemActorOf(SupervisorActor.props(this), "flower");
+//          this.supervierActor = getActorSystem().actorOf(SupervisorActor.props(this), "flower");
         }
       }
     }
