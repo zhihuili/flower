90aec2df09774a811b02c88c0054d88d8d078da5
leeyazhou <lee_yazhou@163.com>
2019-07-09 17:23:09 +0800

    add module flower.config

--- flower.config/src/main/java/com/ly/train/flower/config/RegistryConfig.java
+++ flower.config/src/main/java/com/ly/train/flower/config/RegistryConfig.java
@@ -13,10 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * 
- */
-package com.ly.train.flower.registry.config;
+package com.ly.train.flower.config;
 
 import java.io.Serializable;
 import java.util.HashMap;
