3714cbd74fa16578748545cacafbffd6013a6c69
leeyazhou <lee_yazhou@163.com>
2019-10-18 16:48:53 +0800

    add FlowerExtension

---
+++ flower.core/src/main/java/com/ly/train/flower/core/akka/extension/FlowerExtension.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright © 2019 同程艺龙 (zhihui.li@ly.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ly.train.flower.core.akka.extension;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import akka.actor.ActorSystem;
+import akka.actor.ExtendedActorSystem;
+import akka.actor.Extension;
+
+/**
+ * @author leeyazhou
+ */
+public class FlowerExtension implements Extension {
+  ActorSystem actorSystem;
+  private AtomicInteger counter = new AtomicInteger();
+
+  public FlowerExtension(ExtendedActorSystem system) {
+    this.actorSystem = system;
+  }
+
+  public void increase() {
+    System.err.println(this + "统计邮箱个数：" + counter.incrementAndGet());
+  }
+}
